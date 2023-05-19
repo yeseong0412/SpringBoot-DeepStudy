2de591bf6dc35ed874b9b8df43af9f33c49d01cc
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-05-19 15:11:20 +0900

    5/19 (이것저것 추가함)

--- src/main/java/com/start/myHome/controller/BoardController.java
+++ src/main/java/com/start/myHome/controller/BoardController.java
@@ -1,14 +1,43 @@
 package com.start.myHome.controller;
 
+import com.start.myHome.model.Board;
+import jakarta.validation.Valid;
+import org.springframework.ui.Model;
+import com.start.myHome.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
+    @Autowired
+    private BoardRepository boardRepository;
     @GetMapping("/list")
-    public String list(){
+    public String list(Model model){
+        List<Board> boards= boardRepository.findAll();
+        model.addAttribute("boards" , boards);
         return "board/list";
+    }
+    @GetMapping("/form")
+    public String form(Model model, @RequestParam(required = false) Long id){
+        if(id == null){
+            model.addAttribute("board", new Board());
+        } else {
+            Board board = boardRepository.findById(id).orElse(null);
+            model.addAttribute("board" , board);
+        }
+        return "board/form";
+    }
+    @PostMapping("/form")
+    public String greetingSubmit(@Valid Board board, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()){
+            return "board/form";
+        }
+        boardRepository.save(board);
+        return "redirect:/board/list";
     }
 }
---
+++ src/main/java/com/start/myHome/model/Board.java
@@ -0,0 +1,23 @@
+package com.start.myHome.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+
+@Entity
+@Data
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    @Size(min=2, max=30)
+    private String title;
+    private String content;
+
+}
