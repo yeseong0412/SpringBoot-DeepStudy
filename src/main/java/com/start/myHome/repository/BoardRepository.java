4102e6f170efa1c45564f049889d1ddb75f40b72
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-05-21 16:12:12 +0900

    5/19 (검색 API 구현중)

--- src/main/java/com/start/myHome/repository/BoardRepository.java
+++ src/main/java/com/start/myHome/repository/BoardRepository.java
@@ -3,5 +3,9 @@ package com.start.myHome.repository;
 import com.start.myHome.model.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board,Long> {
+    List<Board> findByTitle(String title);
+    List<Board> findByTitleorContent(String title, String content);
 }
