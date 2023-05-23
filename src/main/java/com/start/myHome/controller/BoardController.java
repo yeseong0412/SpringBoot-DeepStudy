d4fb3c27db5e4c28c148129741823de2cd38469e
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-05-23 21:59:45 +0900

    5/23 (페이지 버튼구현)

--- src/main/java/com/start/myHome/controller/BoardController.java
+++ src/main/java/com/start/myHome/controller/BoardController.java
@@ -6,6 +6,7 @@ import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.ui.Model;
 import com.start.myHome.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,11 +24,12 @@ public class BoardController {
     @Autowired
     private BoardValidator boardValidator;
     @GetMapping("/list")
-    public String list(Model model,Pageable pageable){
+    public String list(Model model, @PageableDefault(size = 2) Pageable pageable){
         Page<Board> boards= boardRepository.findAll(pageable);
-        int startPage = Math.max(0, boards.getPageable().getPageNumber() -4);
-        model.addAllAttributes("startPage", startPage);
-        model.addAllAttributes("startPage", endPage);
+        int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
+        int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber()+4);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
         model.addAttribute("boards" , boards);
         return "board/list";
     }
