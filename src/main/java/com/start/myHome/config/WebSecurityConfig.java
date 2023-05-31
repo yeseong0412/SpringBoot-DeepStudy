9fd6c88d46914d57f2c38e81446c1e22a4ad4cf3
yeseong0412 <yeseong0412@dgsw.hs.kr>
2023-05-31 23:43:03 +0900

    5/31 (스프링시큐리티 계속 구현중 [일부구현] )

--- src/main/java/com/start/myHome/config/WebSecurityConfig.java
+++ src/main/java/com/start/myHome/config/WebSecurityConfig.java
@@ -1,26 +1,32 @@
 package com.start.myHome.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-
+    @Autowired
+    private DataSource dataSource;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/", "/home").permitAll()
                         .anyRequest().authenticated()
-                        .and()
                 )
                 .formLogin((form) -> form
                         .loginPage("/login")
@@ -29,5 +35,22 @@ public class WebSecurityConfig {
                 .logout((logout) -> logout.permitAll());
 
         return http.build();
+    }
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(passwordEncoder())
+                .usersByUsernameQuery("select username,password,enabled "
+                        + "from user "
+                        + "where username = ?")
+                .authoritiesByUsernameQuery("select username, name "
+                        + "from authorities "
+                        + "where email = ?");
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
